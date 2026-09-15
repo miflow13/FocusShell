122251ab1dbd78683f35812e7fe986ba90a11de8
Mika Flowers <pizzafan513@gmail.com>
2026-09-15 10:59:44 -0400

    fix: compose ToolbarView instead of subclassing final type

--- src/focusshell/ui/service_view.py
+++ src/focusshell/ui/service_view.py
@@ -12,12 +12,19 @@ from gi.repository import Adw, Gtk  # noqa: E402
 from focusshell.providers.base import PlaybackProvider
 
 
-class ServiceView(Adw.ToolbarView):
+class ServiceView(Gtk.Box):
     """Keep service-specific web content behind native FocusShell chrome."""
 
     def __init__(self, provider: PlaybackProvider) -> None:
-        super().__init__()
+        super().__init__(orientation=Gtk.Orientation.VERTICAL)
         self._provider = provider
+
+        self.set_hexpand(True)
+        self.set_vexpand(True)
+
+        toolbar = Adw.ToolbarView()
+        toolbar.set_hexpand(True)
+        toolbar.set_vexpand(True)
 
         header = Adw.HeaderBar()
 
@@ -26,8 +33,9 @@ class ServiceView(Adw.ToolbarView):
         reload_button.connect("clicked", self._on_reload_clicked)
         header.pack_end(reload_button)
 
-        self.add_top_bar(header)
-        self.set_content(provider.widget)
+        toolbar.add_top_bar(header)
+        toolbar.set_content(provider.widget)
+        self.append(toolbar)
 
     def _on_reload_clicked(self, _button: Gtk.Button) -> None:
         self._provider.reload()
